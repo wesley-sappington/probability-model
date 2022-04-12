4e3a7d5aeff5acc8c48a4c2806fc1088691a5b43
wesley-sappington <103490852+wesley-sappington@users.noreply.github.com>
2022-04-12 05:40:27 -0400

    Renamed some variables for easier comprehensibility

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const settings = { //General configuration settings for the program (modify this
     removeZeroFish: true, //Automatically remove listings of fish populations (colors) from the UI that have been reduced to zero (false will keep those populations listed as 0)
   },
   starting: {
-    possibleColors: ["orange", "lightgreen"], //Possible colors for the fish to start
+    colors: ["orange", "lightgreen"], //Possible colors for the fish to start
     min:8,
     max:14,
   },
@@ -50,6 +50,7 @@ const settings = { //General configuration settings for the program (modify this
     dialogueEnabled: true,
     dialogueLines_catch: ["Woah, a big one!", "I feel a nibble!", "Wow, what a beauty!"],
     dialogueLines_bucket: ["A great addition to my aquarium.", "What a catch!", "Great job!"],
+    dialogueDuration: 1000,
 
     //Colors
     skinColor: "#FFCC99",
@@ -81,7 +82,6 @@ const settings = { //General configuration settings for the program (modify this
     rodHandleLength: 50,
     rodHeightOverWater: 200,
 
-    dialogueDuration: 1000,
   },
   //Import settings into an object
   import: function(destination, source) {
@@ -97,7 +97,7 @@ function rand(min, max) { //Inclusive, exclusive
 var gameObjects = { //Stores the classes used to create objects in the game
   tank: function(xpos, ypos, width, height) { //The class that manages the bodies (tanks) of water
 
-    //Put constructor parameters into the class, in case they need to be modified later (ex. "myTank.width *=2 would double the width of the tank")
+    //Put constructor parameters into the class, in case they need to be modified later (ex. "myTank.width *=2" would double the width of the tank"
     this.xpos = xpos;
     this.ypos = ypos;
     this.width = width;
@@ -106,12 +106,12 @@ var gameObjects = { //Stores the classes used to create objects in the game
     //Set default configurations
     this.drawBorders = true;
     this.fish = [];
-    this.fishScoreboard = {};
+    this.fishQuantities = {};
     this.canPickUp = this.canPutDown = true;
 
     settings.import(this, settings.tank) //We copy from settings for flexibility - so that the instance can be reconfigured on an individual basis, if needed
 
-    //Keep track of all the tanks in the "game", for detecting mouse clicks & mouse hober
+    //Keep track of all the tanks in the "game", for detecting mouse clicks & mouse hover
     if(gameObjects.allTanks === undefined) gameObjects.allTanks = [];
     gameObjects.allTanks.push(this)
 
@@ -140,7 +140,7 @@ var gameObjects = { //Stores the classes used to create objects in the game
         }
       }
 
-      //Render the mousover highlight
+      //Render the mouse over highlight
       if(this.isMouseOver()) {
         if((this.canPickUp && grabbedFish === undefined) || (this.canPutDown && grabbedFish !== undefined)) { //Only render if settings allow, and the user is in the right mode (has or has not grabbed a fish)
           fill(this.highlightColor)
@@ -174,7 +174,7 @@ var gameObjects = { //Stores the classes used to create objects in the game
       }
 
       //Calculate which direction the fish should be facing. True is right, false is left.
-      fish.rotation = (fish.xpos > this.xpos + (this.width / 2)) * 180 //Here, the fish will go right if it's on the right side of the tank, or left if it's on the left side of the tank
+      fish.rotation = (fish.xpos > this.xpos + (this.width / 2)) * 180 //The fish will go right if it's on the right side of the tank, or left if it's on the left side of the tank
 
       fish.wander = true; //Allow the fish to swim back and forth
       fish.lowerSwimBound = this.xpos; //How far it can swim to the left
@@ -182,9 +182,9 @@ var gameObjects = { //Stores the classes used to create objects in the game
 
       this.fish.push(fish) //Add the fish to the tank array, so that the tank can keep track of it
 
-      //Keep track of the quantity of a certain color of fish
-      if(this.fishScoreboard[fish.color] === undefined) this.fishScoreboard[fish.color] = 0; //Add to the fish scoreboard
-      this.fishScoreboard[fish.color]++;
+      //Keep track of the quantity of each fish color
+      if(this.fishQuantities[fish.color] === undefined) this.fishQuantities[fish.color] = 0;
+      this.fishQuantities[fish.color]++;
     }
     this.removeFish = function(fish) {
 
@@ -193,11 +193,11 @@ var gameObjects = { //Stores the classes used to create objects in the game
       fish.lowerSwimBound = fish.upperSwimBound = undefined; //Remove this data, because it is only relevant to the tank the fish is no longer in!
 
       //Subtract from the fish total
-      if(this.fishScoreboard[fish.color] !== undefined) {
-        this.fishScoreboard[fish.color]--;
-        if(settings.features.removeZeroFish && this.fishScoreboard[fish.color] <= 0) delete this.fishScoreboard[fish.color];
+      if(this.fishQuantities[fish.color] !== undefined) {
+        this.fishQuantities[fish.color]--;
+        if(settings.features.removeZeroFish && this.fishQuantities[fish.color] <= 0) delete this.fishQuantities[fish.color];
       }
-      this.fish.splice(this.fish.indexOf(fish), 1);
+      this.fish.splice(this.fish.indexOf(fish), 1); //Ultimately removes the fish from the tank's domain
     }
     this.isMouseOver = function() {
       if(mouseX > this.xpos && mouseX < this.xpos + this.width && mouseY > this.ypos && mouseY < this.ypos + this.height) {
@@ -213,7 +213,7 @@ var gameObjects = { //Stores the classes used to create objects in the game
 
     //Set default configurations
     this.rotation = 0;
-    this.wander = false; //This denotes if the fish should try to "wander" idley, e.g. swim back and forth within its tank
+    this.wander = false; //This denotes if the fish should try to "wander" idley - swim back and forth within its tank
     this.canPickUp = true;
 
     settings.import(this, settings.fish) //We copy from settings for flexibility - so that the instance can be reconfigured on an individual basis, if needed
@@ -421,7 +421,7 @@ function setup() {
   fisherman = new gameObjects.fisherman(settings.pondWidth + 15, settings.canvasSize - settings.dock.height - (settings.fisherman.torsoWidth / 2), pond, bucket)
 
   //Create the "Add Fish" user interface (Does not need to be continuously rendered)
-  if(settings.features.addFish) buildAddFish();
+  if(settings.features.addFish) buildAddFishInterface();
 
   //Populate the pond with some fish, to start
   populateStarting(pond)
@@ -450,7 +450,7 @@ function draw() {
   //If the user is holding a fish, render that
   if(grabbedFish !== undefined) {
     grabbedFish.xpos = mouseX;
-    grabbedFish.ypos = mouseY;
+    grabbedFish.ypos = mouseY - (grabbedFish.width / 2);
     grabbedFish.update()
 
     fisherman.reeling = grabbedFish;
@@ -485,7 +485,7 @@ function renderProbabilityInterface(pond, x, y) {
 
     rowPositionY += settings.interface.tableRowHeight; //Move down to the first row position
 
-    for(var color in pond.fishScoreboard) { //Iterate through all available colors
+    for(var color in pond.fishQuantities) { //Iterate through all available colors
       textSize(14);
       fill(color);
 
@@ -493,13 +493,13 @@ function renderProbabilityInterface(pond, x, y) {
       text(color.toUpperCase().replace("LIGHT", ""), x, rowPositionY);
 
       //Render collumn 1 - the quantity
-      text(pond.fishScoreboard[color], x + (settings.interface.tableSpacing * 2), rowPositionY);
+      text(pond.fishQuantities[color], x + (settings.interface.tableSpacing * 2), rowPositionY);
 
       //Render collumn 2 - the fractional probability
-      text(pond.fishScoreboard[color] + "/" + pond.fish.length, x + (settings.interface.tableSpacing * 3), rowPositionY);
+      text(pond.fishQuantities[color] + "/" + pond.fish.length, x + (settings.interface.tableSpacing * 3), rowPositionY);
 
       //Render collumn 3 - the percent chance
-      var percent = ((pond.fishScoreboard[color] / pond.fish.length) * 100)
+      var percent = ((pond.fishQuantities[color] / pond.fish.length) * 100)
       if(percent > 1.00) percent = Math.round(percent) //Round the percentage to a whole number, unless it is only a decimal
       else if(percent > 0.00) percent = percent.toFixed(2); //If the percentage is a decimal less than 1, show to the hundreths place
 
@@ -511,7 +511,7 @@ function renderProbabilityInterface(pond, x, y) {
   } else text("The pond is empty. Add some fish below!", x, y);
 }
 
-function buildAddFish() { //This function builds the "Add Fish" button and its associated color buttons. Note that this only needs to be run once, since buttons are stored directly in HTML DOM.
+function buildAddFishInterface() { //This function builds the "Add Fish" button and its associated color buttons. Note that this only needs to be run once, since buttons are stored directly in HTML DOM.
   const buttonRowY = settings.canvasSize - 40;
 
   //Create a sample fish to showcase which color is currently loaded for insertion
@@ -544,7 +544,7 @@ function buildAddFish() { //This function builds the "Add Fish" button and its a
 //Fills the pond with a random assortment of fish
 function populateStarting(pond) {
   for(var i = 0; i <= rand(settings.starting.min, settings.starting.max + 1); i++) {
-    var color = settings.starting.possibleColors[rand(0, settings.starting.possibleColors.length)]; //Choose a random color from the list of possible colors
+    var color = settings.starting.colors[rand(0, settings.starting.colors.length)]; //Choose a random color from the list of possible colors
 
     var f = new gameObjects.fish(color)
     pond.addFish(f);
